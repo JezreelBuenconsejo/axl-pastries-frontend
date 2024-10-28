7b3eda961468fa4311cc2b43886dfe03cc8a29d8
JezreelBuenconsejo <b.jezreel@yahoo.com>
2024-10-28 17:35:53 +0800

    Update featured products carousel

--- src/components/custom/FeaturedProducts/FeaturedProducts.tsx
+++ src/components/custom/FeaturedProducts/FeaturedProducts.tsx
@@ -1,33 +1,41 @@
 "use client";
 import React from "react";
 import FeaturedProductCard from "./FeaturedProductCard";
-import { Carousel, CarouselContent, CarouselItem } from "@/components/ui/carousel";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
 import { Cake } from "@/types/cake";
 
 type FeaturedProductsProps = {
-	products: Cake[];
+  products: Cake[];
 };
-const FeaturedProducts: React.FC<FeaturedProductsProps> = props => {
-	return (
-		<section className="mx-auto my-24  flex w-full max-w-[1440px] flex-col pl-5 lg:px-20">
-			<Carousel>
-				<CarouselContent>
-					{React.Children.toArray(
-						props.products.map(item => (
-							<CarouselItem  className="px-5 sm:basis-[250px] md:basis-[350px]">
-								<FeaturedProductCard
-									img={item.image_url}
-									category={"Cake"}
-									name={item.name}
-									price={item.price}
-								/>
-							</CarouselItem>
-						))
-					)}
-				</CarouselContent>
-			</Carousel>
-		</section>
-	);
+const FeaturedProducts: React.FC<FeaturedProductsProps> = (props) => {
+  return (
+    <section className="mx-auto my-24  flex w-full max-w-[1440px] flex-col">
+      <Carousel
+        opts={{
+          align: "start",
+        }}
+      >
+        <CarouselContent className="first:pl-5 md:first:pl-20">
+          {React.Children.toArray(
+            props.products.map((item) => (
+              <CarouselItem className="px-5 sm:basis-[250px] md:basis-[350px]">
+                <FeaturedProductCard
+                  img={item.image_url}
+                  category={"Cake"}
+                  name={item.name}
+                  price={item.price}
+                />
+              </CarouselItem>
+            ))
+          )}
+        </CarouselContent>
+      </Carousel>
+    </section>
+  );
 };
 
 export default FeaturedProducts;
